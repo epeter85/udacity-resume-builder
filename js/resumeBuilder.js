0b1884cb2bffb5a9f79fa082b6921fa39fcb493a
Erik Peterson <erik@erik-id.com>
2018-12-29 17:05:37 -0500

    WIP education data view

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  var data = {
+  var model = {
 
     bio: {
       name: 'Tom Tommy',
@@ -14,25 +14,7 @@ $(function() {
       },
       welcomeMessage: 'Hiya Peoples',
       skills: ['HTML', 'CSS', 'Making Coffee'],
-      biopic: 'www.tom.com',
-      display: function() {
-
-          console.log('bio display');
-          this.$header = $('#header');
-          this.$topContacts = $('#topContacts');
-          var formattedName = HTMLheaderName.replace("%data%", this.name);
-          var formattedRole = HTMLheaderRole.replace("%data%", this.role);
-          var formattedMobile = HTMLmobile.replace("%data%", this.contacts.mobile);
-          var formattedEmail = HTMLemail.replace("%data%", this.contacts.email);
-          var formattedTwitter = HTMLtwitter.replace("%data%", this.contacts.twitter);
-          var formattedGithub = HTMLgithub.replace("%data%", this.contacts.github);
-          var formattedLocation = HTMLlocation.replace("%data%", this.contacts.location);
-          //var formattedBioPic = HTMLbioPic.replace("%data%", this.contacts.location);
-          var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", this.welcomeMessage);
-
-          this.$header.append(formattedName, formattedRole, formattedWelcomeMsg);
-          this.$topContacts.append(formattedMobile, formattedEmail, formattedTwitter, formattedGithub, formattedLocation);
-      }
+      biopic: 'www.tom.com'
     },
 
     education: {
@@ -109,17 +91,68 @@ $(function() {
 
   var octopus = {
 
+    init: function() {
+        bioView.init();
+        educationView.init();
+    }
+
   };
 
-  var headerView = {
-     init: function() {
-          var formattedLocation = HTMLheaderName.replace("%data%", model.bio.name);
-          this.$header = $('#header');
-          this.$header.append(formattedLocation);
-      }
+  var bioView = {
+
+    init: function() {
+
+      this.$header = $('#header');
+      this.$topContacts = $('#topContacts');
+      var formattedName = HTMLheaderName.replace("%data%", model.bio.name);
+      var formattedRole = HTMLheaderRole.replace("%data%", model.bio.role);
+      var formattedMobile = HTMLmobile.replace("%data%", model.bio.contacts.mobile);
+      var formattedEmail = HTMLemail.replace("%data%", model.bio.contacts.email);
+      var formattedTwitter = HTMLtwitter.replace("%data%", model.bio.contacts.twitter);
+      var formattedGithub = HTMLgithub.replace("%data%", model.bio.contacts.github);
+      var formattedLocation = HTMLlocation.replace("%data%", model.bio.contacts.location);
+      //var formattedBioPic = HTMLbioPic.replace("%data%", model.bio.contacts.location);
+      var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", model.bio.welcomeMessage);
+
+      var formattedSkills = '';
+      model.bio.skills.forEach(function(skill) {
+          formattedSkills += HTMLskills.replace("%data%", skill);
+      });
+
+      this.$header.append(formattedName, formattedRole, formattedWelcomeMsg);
+      this.$topContacts.append(formattedMobile, formattedEmail, formattedTwitter, formattedGithub, formattedLocation);
+      this.$header.append(HTMLskillsStart);
+      $('#skills').append(formattedSkills);
+
+    }
   };
 
-  //headerView.init();
-  data.bio.display();
+  var educationView = {
+
+    init: function() {
+
+      this.$education = $('#education');
+
+      var formattedSchools = '';
+      model.education.schools.forEach(function(school) {
+          formattedSchools += HTMLschoolName.replace("%data%", school.name);
+          formattedSchools += HTMLschoolLocation.replace("%data%", school.location);
+          formattedSchools += HTMLschoolDegree.replace("%data%", school.degree);
+          school.majors.forEach(function(major) {
+            formattedSchools += HTMLschoolMajor.replace("%data%", major);
+          });
+
+          formattedSchools += HTMLschoolDates.replace("%data%", school.dates);
+      });
+
+      console.log(formattedSchools);
+
+      this.$education.append(HTMLschoolStart);
+      $('.education-entry').append(formattedSchools);
+
+    }
+  };
+
+  octopus.init();
 
 }());
